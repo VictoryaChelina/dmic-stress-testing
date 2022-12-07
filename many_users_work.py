7e63b34a5b5c5215290fd7c4f3f956790177cba8
Victorya <v.chelina@ispras.ru>
2022-12-07 10:48:13 +0300

    Сделан класс RandUser

--- many_users_work.py
+++ many_users_work.py
@@ -3,7 +3,6 @@ import time
 import os
 import random_marker as rm
 import infi.clickhouse_orm as ico
-from infi.clickhouse_orm.fields import IPv4Field
 from random import randint, random
 from enum import Enum
 
@@ -40,7 +39,7 @@ TIME_FACT = 1  # Промежутки между фактами маркиров
 '''
 
 ROWS_NUM = 5  # Количество генерируемых строк от одного пользователя
-USERS_NUM = 1 # Количество пользователей
+USERS_NUM = 2 # Количество пользователей
 BATCH_SIZE = 5  # Количество строк отправляемых за одну загрузку (в оригинале 100)
 
 
@@ -62,94 +61,24 @@ class ScreenmarkFact(ico.Model):
     #operation_type = ico.Enum8Field(Operation)
 
 
-
-# Генератор пользователей (пока пользователи генерируются поочереди)
-def gen_users():
-    users = []
-    for i in range(USERS_NUM):
-        user = gen_rows_one_user()
-        users += user
-    return users
-
-
-# Генератор рандомных строк для таблиц screenmarkfact (один пользователь)
-# report_time нужно изменить далее
-# @profile
-def gen_rows_one_user():
-    rows_one_user = []
-    user_name = rm.rand_user()
-    user_domain = rm.rand_domain()
-    department = rm.rand_department()
-    root_disk_serial = rm.rand_disk()
-    marker = rm.rand_marker(department, root_disk_serial, user_name, user_domain)
-    ipv4_address = rm.rand_ip()
-    hw_address = rm.rand_hw()[:17]
-    for i in range(ROWS_NUM):
-        row = ScreenmarkFact(
-            dt = datetime.date.today(),\
-            dtm = datetime.datetime.today(),\
-            report_time = datetime.datetime.today(),\
-            user_name=user_name,\
-            user_domain=user_domain,\
-            department=department,\
-            root_disk_serial=root_disk_serial,\
-            marker=marker,\
-            ipv4_address=ipv4_address,\
-            hw_address=hw_address)
-            #operation_type = Operation.SCREEN if random() < 0.95 else Operation.PRINT)
-        rows_one_user.append(row)
-        # time.sleep(TIME_FACT)
-    return rows_one_user
-
-
-def make_path():
-    path = os.getcwd()
-    path = os.mkdir(path + f'\\log\\{str(rm.rand_folder())}\\')
-    return str(path)
-
-
-# Генератор файла screenmark_log
-def one_user_screen_log():
-    path = make_path()
-    with open(path + "screenmark_log", "w") as log:
-        rows = gen_rows_one_user()
-        for row in rows:
-            log.write(
-                f'{str(row.dtm)[:19]},{str(row.dtm)[20:23]} - event_api.py: INFO - screen-marking;' +
-                f'{row.user_name};' + 
-                f'{row.user_domain};' +
-                f'{row.marker};' +
-                f'{row.department};' +
-                f'{row.root_disk_serial};' + 
-                f'{row.ipv4_address};' +
-                f'{row.hw_address}' + '\n')
-
-
-# Генератор файла printmark_log
-def one_user_print_log():
-    path = make_path()
-    with open(path + "screenmark_log", "w") as log:
-        rows = gen_rows_one_user()
-        for row in rows:
-            log.write(
-                f'{str(row.dtm)[:19]},{str(row.dtm)[20:23]} - event_api.py: INFO - print-marking;' + #  надо уточнить (посмотреть в printmark_log)
-                f'{row.user_name};' + 
-                f'{row.user_domain};' +
-                f'{row.marker};' +
-                f'{row.department};' +
-                f'{row.root_disk_serial};' + 
-                f'{row.ipv4_address};' +
-                f'{row.hw_address}' + '\n')
-
-
 # Класс отправки псевдологов
-class DMSpectator:
-    LOGFILE = 'dmspectator_log'
+class SpectatorTesting:
 
-    # Подключение к серверу
-    def connect(self):
+    CONNECTIONS = {} # Словарь id: ico.Database
+    USERS = {}  # Словарь id: rm.RandUser 
+
+    # Генерируется заданное число пользователей
+    def gen_users(self):
+        for user in range(USERS_NUM):
+            user = rm.RandUser()
+            self.USERS[user.user_id()] = user
+            self.CONNECTIONS[user.user_id()] = None
+
+    # Подключение к базе
+    def connect(self, id):
         try:
-            department_number = rm.rand_department()
+            user = self.USERS[id]
+            department_number = user.department
             uname_ = f'department{department_number:05}'
             pass_ = f'pass{department_number:05}'
 
@@ -158,18 +87,23 @@ class DMSpectator:
                 db_url=DB_URL,
                 username=uname_,
                 password=pass_)
-            print("ОК")
+            print(f'{id}: Подключился базе')
             return True
         except Exception as ex_:
-            print("Подключение не удалось")
+            print(f'{id}: Происходит подключение')
         return False
 
-    def process(self):
+    def process(self, id):
         self.connected = False
         while not self.connected:
-            self.connected = self.connect()
+            self.connected = self.connect(id=id)
             if not self.connected:
                 time.sleep(CONNECTED_INT)
+
+    # Пользователи подключаются к базе
+    def connect_users(self):
+        for id in self.USERS.keys():
+            self.process(id=id)            
     
     # Отправка псевдологов
     # @profile
@@ -178,18 +112,103 @@ class DMSpectator:
         print("Отправка логов")
         self.db.insert(rows, BATCH_SIZE)
 
+    def entr_point(self):
+        self.gen_users()
+        self.connect_users()
+
 
 def main():
-    dms = DMSpectator()
-    dms.process()
-    t_start = time.time()
-    print(dms.push_update())
-    print(time.time() - t_start)
+    test = SpectatorTesting()
+    test.entr_point()
+    # dms = SpectatorTesting()
+    # dms.process()
+    # t_start = time.time()
+    # print(dms.push_update())
+    # print(time.time() - t_start)
     # one_user_screen_log()
     # one_user_print_log()
-
     return 0
 
 
 if __name__ == '__main__':
     main()
+
+
+# # Генератор пользователей (пока пользователи генерируются поочереди)
+# def gen_users():
+#     users = []
+#     for i in range(USERS_NUM):
+#         user = gen_rows_one_user()
+#         users += user
+#     return users
+
+
+# # Генератор рандомных строк для таблиц screenmarkfact (один пользователь)
+# # report_time нужно изменить далее
+# # @profile
+# def gen_rows_one_user():
+#     rows_one_user = []
+#     user_name = rm.rand_user()
+#     user_domain = rm.rand_domain()
+#     department = rm.rand_department()
+#     root_disk_serial = rm.rand_disk()
+#     marker = rm.rand_marker(department, root_disk_serial, user_name, user_domain)
+#     ipv4_address = rm.rand_ip()
+#     hw_address = rm.rand_hw()[:17]
+#     for i in range(ROWS_NUM):
+#         row = ScreenmarkFact(
+#             dt = datetime.date.today(),\
+#             dtm = datetime.datetime.today(),\
+#             report_time = datetime.datetime.today(),\
+#             user_name=user_name,\
+#             user_domain=user_domain,\
+#             department=department,\
+#             root_disk_serial=root_disk_serial,\
+#             marker=marker,\
+#             ipv4_address=ipv4_address,\
+#             hw_address=hw_address)
+#             #operation_type = Operation.SCREEN if random() < 0.95 else Operation.PRINT)
+#         rows_one_user.append(row)
+#         # time.sleep(TIME_FACT)
+#     return rows_one_user
+
+
+
+# def make_path():
+#     path = os.getcwd()
+#     path = os.mkdir(path + f'\\log\\{str(rm.rand_folder())}\\')
+#     return str(path)
+
+
+# # Генератор файла screenmark_log
+# def one_user_screen_log():
+#     path = make_path()
+#     with open(path + "screenmark_log", "w") as log:
+#         rows = gen_rows_one_user()
+#         for row in rows:
+#             log.write(
+#                 f'{str(row.dtm)[:19]},{str(row.dtm)[20:23]} - event_api.py: INFO - screen-marking;' +
+#                 f'{row.user_name};' + 
+#                 f'{row.user_domain};' +
+#                 f'{row.marker};' +
+#                 f'{row.department};' +
+#                 f'{row.root_disk_serial};' + 
+#                 f'{row.ipv4_address};' +
+#                 f'{row.hw_address}' + '\n')
+
+
+# # Генератор файла printmark_log
+# def one_user_print_log():
+#     path = make_path()
+#     with open(path + "screenmark_log", "w") as log:
+#         rows = gen_rows_one_user()
+#         for row in rows:
+#             log.write(
+#                 f'{str(row.dtm)[:19]},{str(row.dtm)[20:23]} - event_api.py: INFO - print-marking;' + #  надо уточнить (посмотреть в printmark_log)
+#                 f'{row.user_name};' + 
+#                 f'{row.user_domain};' +
+#                 f'{row.marker};' +
+#                 f'{row.department};' +
+#                 f'{row.root_disk_serial};' + 
+#                 f'{row.ipv4_address};' +
+#                 f'{row.hw_address}' + '\n')
