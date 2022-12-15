439500a8fdbfd29a14ef67d19ecd417b8c2cc225
Victorya <v.chelina@ispras.ru>
2022-12-15 15:53:31 +0300

    #4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(
-    name="many_users_work",
+    name="dmic-stress-testing",
     version="0",
     packages=["dmic-stress-testing"],
     include_package_data=True,
@@ -12,15 +12,15 @@ setup(
     description="Stress tessting of DMIC server",
 
     install_requires=[
-        'marker_generator',
+        'infi.clickhouse_orm',
+        'threading',
+        'concurrent.futures',
+        'import numpy as np'
     ],
 
-    # entry_points={
-    #     'console_scripts': [
-    #         'dm-decode-marker = marker_generator.decode:main',
-    #         'dm-gen-marker = marker_generator.main:gen',
-    #         'dm-get-marker = marker_generator.main:get',
-    #         'dm-gen-decode = marker_generator.main:gen_decode'
-    #     ]
-    # }
+    entry_points={
+         'console_scripts': [
+            'make-stress = dmic-stress-testing.many_users_work:main'
+        ]
+    }
 )
