d76f2b770668c7ac17b07b3f415d6a47d2d06ce4
Victorya <v.chelina@ispras.ru>
2022-12-16 17:12:39 +0300

    in progress #3

--- dmic_stress_testing/async_many_users_work.py
+++ dmic_stress_testing/async_many_users_work.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 from time import perf_counter
-import random_marker as rm
+import dmic_stress_testing.random_marker as rm
 from random import randint, random
 from enum import Enum
 import traceback
@@ -130,69 +130,46 @@ class SpectatorTesting:
                 self.last_push_time[id] = datetime.datetime.today() - datetime.timedelta(minutes=1)
                 self.user_rows_count[id] = 0
                 # user.user_info()
-                self.rows_const_part[id] = Row(
-                    user_name=user.user_name, 
-                    user_domain=user.user_domain,
-                    marker=user.marker,
-                    department=user.department,
-                    root_disk_serial=user.disk,
-                    ipv4_address=user.ip,
-                    hw_address=user.hw)
+                self.rows_const_part[id] = (
+                    datetime.date(1984, 1, 1),
+                    datetime.datetime(1984, 1, 1, 1, 1, 1, 1),
+                    datetime.datetime(1984, 1, 1, 1, 1, 1, 1),
+                    user.user_name, 
+                    user.user_domain,
+                    user.marker,
+                    user.department,
+                    user.disk,
+                    user.ip,
+                    user.hw)
+    
+    # Вставка строки
+    async def insert_row(self, client, id):
+        row = self.rows_const_part[id]
+        await client.execute("INSERT INTO screenmarkfact (*) VALUES", row)
 
-    # # Подключение к базе
-    # async def connect(self, id):
-    #     try:
-    #         user = self.users[id]
-    #         department_number = int(user.department)
-    #         uname_ = f'department{department_number:05}'
-    #         pass_ = f'pass{department_number:05}'
-    #         async with ClientSession() as s:
-    #             client = ChClient(
-    #                 session=s,
-    #                 url=self.configuration['DB_URL'],
-    #                 database='dmic',
-    #                 user=uname_,
-    #                 password=pass_)
-    #         self.connections[id] = client
-    #         logging.info(f'{id} {uname_} {pass_}: Подключился базе')
-    #         return True
-    #     except Exception as ex_:
-    #         logging.info(f'{id} {uname_} {pass_}: Подключение...')
-    #         #traceback.print_exc()
-    #     return False
-
-    # async def process(self, id):
-    #     self.connected = False
-    #     attempts_counter = 0
-    #     start = perf_counter()
-    #     while not self.connected and attempts_counter <= self.configuration['MAX_CONNECTION_ATTEMPTS']:
-    #         attempts_counter += 1
-    #         self.connected = self.connect(id=id)
-    #         if not self.connected:
-    #             time.sleep(self.configuration['CONNECTION_INTERVAL'])
-    #     stop = perf_counter()
-    #     if self.connected == False:
-    #         self.not_connected_users.append(id)
-    #     else:
-    #         self.user_connection_time.append(stop-start)
-
-    # # Пользователи подключаются к базе
-    # async def connect_users(self):
-    #     start = perf_counter()
-    #     for id in self.connections.keys():
-    #         self.process(id)
-    #     stop = perf_counter()
-    #     self.total_user_connection = stop - start
-
-    async def connect_users(self, id, client: ChClient):
-        
+    # Подключение клиента и запуск вставки строк 
+    async def connect_client(self, id):
         async with ClientSession() as s:
+            user = self.users[id]
+            department_number = int(user.department)
+            uname_ = f'department{department_number:05}'
+            pass_ = f'pass{department_number:05}'
             client = ChClient(
                 session=s,
                 url=self.configuration['DB_URL'],
                 database='dmic',
-                user=,
-                password='yuramarkin')
+                user=uname_,
+                password=pass_)
+            futures = [self.insert_row(client=client, id=id) for row in range(self.configuration['ROWS_NUM'])]
+            await asyncio.gather(*futures)
+        logging.info(f'client with id {id} connected')
+    
+    # Цикл по клиентам 
+    async def connect_clients(self):
+        async with ClientSession() as s:
+            futures = [self.connect_client(id=id) for id in self.users.keys()]
+            await asyncio.gather(*futures)
+        logging.info(f'clients done')
 
     # Генерация строк
     def gen_rows(self, id, report_time):
@@ -211,77 +188,55 @@ class SpectatorTesting:
             rows.append(row)
         return rows
 
-    def insertion(self, id, rows):
-        start = perf_counter()
-        self.connections[id].insert(rows, self.configuration['BATCH_SIZE'])
-        stop = perf_counter()
-        self.row_insertion_time.append((stop - start)/len(rows))
-
-    def push_update_one_user(self, id):
-        report_time = datetime.datetime.today()  # Время отправки строк лога с клиента на dmic
-        if report_time - self.last_push_time[id] >= datetime.timedelta(seconds=self.configuration['PUSH_INT']):
-            rows = self.gen_rows(id = id, report_time=report_time)
-            self.user_rows_count[id] += self.configuration['ROWS_NUM']
-            self.insertion(id=id, rows=rows)
-            self.last_push_time[id] = report_time
-
-    # Запускает цикл по connections для отправки логов
-    def pushing_updates(self):
-        start = perf_counter()
-        while True:
-            for id in self.users.keys():
-                self.push_update_one_user(id=id)
-            break
-        stop = perf_counter()
-        self.total_user_push = stop - start
     
-    def metrics(self):
-        row_generation = np.array(self.row_generation_time)
-        average_row_generation = np.average(row_generation)
-        rows_num = row_generation.size
-        total_row_generation = np.sum(row_generation)
+    # def metrics(self):
+    #     row_generation = np.array(self.row_generation_time)
+    #     average_row_generation = np.average(row_generation)
+    #     rows_num = row_generation.size
+    #     total_row_generation = np.sum(row_generation)
 
-        user_connection = np.array(self.user_connection_time)
-        average_user_connection = np.average(user_connection)
-        connections_num = user_connection.size
+    #     user_connection = np.array(self.user_connection_time)
+    #     average_user_connection = np.average(user_connection)
+    #     connections_num = user_connection.size
 
-        row_insertion = np.array(self.row_insertion_time)
-        average_row_insertion = np.average(row_insertion)
+    #     row_insertion = np.array(self.row_insertion_time)
+    #     average_row_insertion = np.average(row_insertion)
 
-        padding = 40
-        print('МЕТРИКИ:\n')
-        print('Threading for push updates:'.ljust(padding), THREAD)
-        print('ThreadPool for connect users:'.ljust(padding), POOL, '\n')
+    #     padding = 40
+    #     print('МЕТРИКИ:\n')
+    #     print('Threading for push updates:'.ljust(padding), THREAD)
+    #     print('ThreadPool for connect users:'.ljust(padding), POOL, '\n')
 
-        print('Number of departments:'.ljust(padding), self.configuration['DEPARTMENT_NUM'])
-        print('Number of users per department:'.ljust(padding), self.configuration['USERS_NUM'])
-        print('Total number of users:'.ljust(padding), self.configuration['USERS_NUM'] * self.configuration['DEPARTMENT_NUM'], '\n')
+    #     print('Number of departments:'.ljust(padding), self.configuration['DEPARTMENT_NUM'])
+    #     print('Number of users per department:'.ljust(padding), self.configuration['USERS_NUM'])
+    #     print('Total number of users:'.ljust(padding), self.configuration['USERS_NUM'] * self.configuration['DEPARTMENT_NUM'], '\n')
 
-        print('Среднее время на генерацию строки:'.ljust(padding), average_row_generation)
-        print('Всего строк было сгенерировано:'.ljust(padding), rows_num)
-        print('Всего времени потрачено:'.ljust(padding), total_row_generation, '\n')
+    #     print('Среднее время на генерацию строки:'.ljust(padding), average_row_generation)
+    #     print('Всего строк было сгенерировано:'.ljust(padding), rows_num)
+    #     print('Всего времени потрачено:'.ljust(padding), total_row_generation, '\n')
 
-        print(average_user_connection)
-        print('Среднее время подключения к базе:'.ljust(padding), average_user_connection)
-        print('Всего подключений:'.ljust(padding), connections_num)
-        print('Всего времени потрачено:'.ljust(padding), self.total_user_connection, '\n')
+    #     print(average_user_connection)
+    #     print('Среднее время подключения к базе:'.ljust(padding), average_user_connection)
+    #     print('Всего подключений:'.ljust(padding), connections_num)
+    #     print('Всего времени потрачено:'.ljust(padding), self.total_user_connection, '\n')
 
-        print('Среднее время вставки строки в базу:'.ljust(padding), average_row_insertion)
-        print('Всего времени = среднее * кол-во строк'.ljust(padding), average_row_insertion * rows_num)
-        print('Всего времени потрачено:'.ljust(padding), self.total_user_push, '\n')
+    #     print('Среднее время вставки строки в базу:'.ljust(padding), average_row_insertion)
+    #     print('Всего времени = среднее * кол-во строк'.ljust(padding), average_row_insertion * rows_num)
+    #     print('Всего времени потрачено:'.ljust(padding), self.total_user_push, '\n')
 
     def entr_point(self):
-        start_gen = perf_counter()
+        #start_gen = perf_counter()
         self.gen_users()  # Создаются пользователи, подключения и подготавливаются неизменяемые части строк
-        end_gen = perf_counter()
-        logging.warning(f'Generated users {len(self.users)} in {end_gen-start_gen} seconds')
-        self.connect_users()  # Пользователи подключаются к базе
-        end_connect = perf_counter()
-        logging.warning(f'Connected users {len(self.users)} in {end_connect-end_gen} seconds')
-        self.pushing_updates()  # В бесконечном цикле пушатся строки от пользователей
-        end_push = perf_counter()
-        logging.warning(f'pushed rows in {end_push-end_connect} seconds')
-        self.metrics()
+        asyncio.run(self.connect_clients())
+        # end_gen = perf_counter()
+        # logging.warning(f'Generated users {len(self.users)} in {end_gen-start_gen} seconds')
+        # self.connect_users()  # Пользователи подключаются к базе
+        # end_connect = perf_counter()
+        # logging.warning(f'Connected users {len(self.users)} in {end_connect-end_gen} seconds')
+        # self.pushing_updates()  # В бесконечном цикле пушатся строки от пользователей
+        # end_push = perf_counter()
+        # logging.warning(f'pushed rows in {end_push-end_connect} seconds')
+        # self.metrics()
 
 
 def read_config():
