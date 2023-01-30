b370691e06584f9019762d7d364de1b1d6874fc2
Victorya <v.chelina@ispras.ru>
2023-01-30 19:11:24 +0300

    подровняла под pep8

--- dmic_stress_testing/many_users_work.py
+++ dmic_stress_testing/many_users_work.py
@@ -17,7 +17,9 @@ from tqdm import tqdm
 THREAD = True
 POOL = True
 
-logging.basicConfig(level=logging.WARNING, handlers=[FileHandler('err_log6.txt')])
+logging.basicConfig(
+    level=logging.WARNING,
+    handlers=[FileHandler('err_log6.txt')])
 
 
 # Модель таблиц
@@ -32,7 +34,6 @@ class ScreenmarkFact(ico.Model):
     root_disk_serial = ico.StringField()
     ipv4_address = ico.IPv4Field()
     hw_address = ico.FixedStringField(length=17)
-    #operation_type = ico.Enum8Field(Operation)
 
     def row_info(self):
         padding = 17
@@ -47,7 +48,7 @@ class ScreenmarkFact(ico.Model):
         print('root_disk_serial:'.ljust(padding), self.root_disk_serial)
         print('ipv4_address:'.ljust(padding), self.ipv4_address)
         print('hw_address:'.ljust(padding), self.hw_address)
-        
+
 
 # Класс отправки псевдологов
 class SpectatorTesting:
@@ -55,15 +56,28 @@ class SpectatorTesting:
         self.configuration = configuration
         self.f = open('./' + self.configuration['LOG'], 'w', newline='')
 
-    connections = {}  # Словарь id: ico.Database (экземпляры подключения)
-    not_connected_users = []  # Список пользователей, которые не смогли подключиться к базе за максимальное число попыток
-    users = {}  # Словарь id: rm.RandUser 
-    rows_const_part = {}  # Словарь id: ScreenmarkFact подготовленная строка для пушинга (неизменяемая часть)
-    last_push_time = {}  # Словарь id: время последней отправки с пользователя
-    user_rows_count = {}  # Словарь id: всего строк отправлено от пользователя
+    # Словарь id: ico.Database (экземпляры подключения)
+    connections = {}
+
+    # Список пользователей, которые не смогли
+    # подключиться к базе за максимальное число попыток
+    not_connected_users = []
+
+    # Словарь id: rm.RandUser
+    users = {}
+
+    # Словарь id: ScreenmarkFact подготовленная строка
+    # для пушинга (неизменяемая часть)
+    rows_const_part = {}
+
+    # Словарь id: время последней отправки с пользователя
+    last_push_time = {}
+
+    # Словарь id: всего строк отправлено от пользователя
+    user_rows_count = {}
 
     # Сбор метрик
-    rows_per_second = []  # Строк в секунду (при каждом добавлении от пользователя)
+    rows_per_second = []
     total_user_push = 0
     insertion_time = 0
     start_connection_time = None
@@ -80,21 +94,23 @@ class SpectatorTesting:
                 id = user.user_id()
                 self.users[id] = user
                 self.connections[id] = None
-                self.last_push_time[id] = datetime.datetime.today() - datetime.timedelta(minutes=1)
+                self.last_push_time[id] = (
+                    datetime.datetime.today()
+                    - datetime.timedelta(minutes=1))
                 self.user_rows_count[id] = 0
                 # user.user_info()
                 self.rows_const_part[id] = ScreenmarkFact(
-                    dt=datetime.datetime(1984, 1, 1, 1, 1, 1, 1), \
-                    dtm=datetime.datetime(1984, 1, 1, 1, 1, 1, 1), \
-                    report_time=datetime.datetime(1984, 1, 1, 1, 1, 1, 1), \
-                    user_name=user.user_name, \
-                    user_domain=user.user_domain, \
-                    marker=user.marker, \
-                    department=user.department, \
-                    root_disk_serial=user.disk, \
-                    ipv4_address=user.ip, \
+                    dt=datetime.datetime(1984, 1, 1, 1, 1, 1, 1),
+                    dtm=datetime.datetime(1984, 1, 1, 1, 1, 1, 1),
+                    report_time=datetime.datetime(1984, 1, 1, 1, 1, 1, 1),
+                    user_name=user.user_name,
+                    user_domain=user.user_domain,
+                    marker=user.marker,
+                    department=user.department,
+                    root_disk_serial=user.disk,
+                    ipv4_address=user.ip,
                     hw_address=user.hw)
-        self.pbar = tqdm(total = len(self.users), desc='Connecting users')
+        self.pbar = tqdm(total=len(self.users), desc='Connecting users')
 
     # Подключение к базе
     def connect(self, id):
@@ -109,10 +125,10 @@ class SpectatorTesting:
                 db_url=self.configuration['DB_URL'],
                 username=uname_,
                 password=pass_)
-            
+
             if self.configuration['ASYNC_INSERT']['ON']:
                 self.db.add_setting(
-                    "async_insert", 
+                    "async_insert",
                     1)
                 self.db.add_setting(
                     "async_insert_max_data_size",
@@ -131,12 +147,14 @@ class SpectatorTesting:
     def process(self, id):
         self.connected = False
         attempts_counter = 0
-        while not self.connected and attempts_counter <= self.configuration['MAX_CONNECTION_ATTEMPTS']:
+        while (not self.connected and
+                attempts_counter <=
+                self.configuration['MAX_CONNECTION_ATTEMPTS']):
             attempts_counter += 1
             self.connected = self.connect(id=id)
             if not self.connected:
                 time.sleep(self.configuration['CONNECTION_INTERVAL'])
-        if self.connected == False:
+        if not self.connected:
             self.not_connected_users.append(id)
         self.pbar.update(1)
 
@@ -152,12 +170,16 @@ class SpectatorTesting:
     # Генерация строк
     def gen_rows(self, id, report_time):
         rows = []
-        mark_time = report_time  # Время записи в лог на клиенте факта маркирования
-        delta = datetime.timedelta(seconds=self.configuration['MARK_INTERVAL'])  # Интервал между фактами маркирования 
-        row = self.rows_const_part[id]  # Подготовленная строка по данному пользователю
+
+        # Время записи в лог на клиенте факта маркирования
+        mark_time = report_time
+
+        # Интервал между фактами маркирования
+        delta = datetime.timedelta(seconds=self.configuration['MARK_INTERVAL'])
+        row = self.rows_const_part[id]
         for i in range(self.configuration['ROWS_NUM'], 0, -1):
-            mark_time = report_time - delta * i  # Меняется время маркирования для записи строки
-            row.dt = mark_time  # В подготовленную строку добавляются отметки времени
+            mark_time = report_time - delta * i
+            row.dt = mark_time
             row.dtm = mark_time
             row.report_time = report_time
             rows.append(row)
@@ -169,28 +191,39 @@ class SpectatorTesting:
             self.last_insertion_time = perf_counter()
             self.total_user_push += self.configuration['ROWS_NUM']
             self.user_rows_count[id] += self.configuration['ROWS_NUM']
-            time_from_start = self.last_insertion_time - self.start_insertion_time
+            time_from_start = (
+                self.last_insertion_time
+                - self.start_insertion_time)
             rps = self.total_user_push / time_from_start
             self.rows_per_second.append(rps)
-            print(f'rps: {rps}', end='\r')
+            # print(f'rps: {rps}', end='\r')
             writer = csv.writer(self.f, delimiter=',', quotechar='|')
             writer.writerow([time_from_start, self.total_user_push, rps])
+            self.pbar.update(1)
         except Exception as ex:
             logging.warning(ex)
-    
+
     def push_update_one_user(self, id):
-        report_time = datetime.datetime.today()  # Время отправки строк лога с клиента на dmic
-        if report_time - self.last_push_time[id] >= datetime.timedelta(seconds=self.configuration['PUSH_INT']):
-            rows = self.gen_rows(id = id, report_time=report_time)
+        # Время отправки строк лога с клиента на dmic
+        report_time = datetime.datetime.today()
+        if (report_time - self.last_push_time[id]
+                >= datetime.timedelta(seconds=self.configuration['PUSH_INT'])):
+            rows = self.gen_rows(id=id, report_time=report_time)
             self.user_rows_count[id] += self.configuration['ROWS_NUM']
             if THREAD:
-                threading.Thread(target=self.insertion, args=(id, rows), daemon=True).start()
+                threading.Thread(
+                    target=self.insertion,
+                    args=(id, rows),
+                    daemon=True).start()
             else:
                 self.insertion(id=id, rows=rows)
             self.last_push_time[id] = report_time
 
     # Запускает цикл по connections для отправки логов
     def loops(self):
+        self.pbar = tqdm(
+            total=len(self.users) * self.configuration['AMOUNT'],
+            desc='Inserting rows')
         for _ in range(self.configuration['AMOUNT']):
             for id in self.users.keys():
                 while threading.active_count() > self.configuration["LIMIT"]:
@@ -206,42 +239,61 @@ class SpectatorTesting:
                 while threading.active_count() > self.configuration["LIMIT"]:
                     continue
                 self.push_update_one_user(id=id)
-    
+
     def metrics(self):
-        average_rps = self.total_user_push / (self.last_insertion_time - self.start_insertion_time)
+        average_rps = self.total_user_push
+        / (self.last_insertion_time - self.start_insertion_time)
 
         padding = 40
         print('МЕТРИКИ:\n')
-        
         print(':', 'LIMIT:'.ljust(padding), self.configuration['LIMIT'])
-        print(':', 'Number of departments:'.ljust(padding), self.configuration['DEPARTMENT_NUM'])
-        print(':', 'Number of users per department:'.ljust(padding), self.configuration['USERS_NUM'])
-        print(':', 'Total number of users:'.ljust(padding), self.configuration['USERS_NUM'] * self.configuration['DEPARTMENT_NUM'])
-        print(':', 'Total number of rows:'.ljust(padding), self.total_user_push, '\n')
-
-        print(':', 'Время на подключение:'.ljust(padding), self.stop_connection_time - self.start_connection_time, '\n')
-
+        print(
+            ':',
+            'Number of departments:'.ljust(padding),
+            self.configuration['DEPARTMENT_NUM'])
+        print(
+            ':',
+            'Number of users per department:'.ljust(padding),
+            self.configuration['USERS_NUM'])
+        print(
+            ':',
+            'Total number of users:'.ljust(padding),
+            self.configuration['USERS_NUM']
+            * self.configuration['DEPARTMENT_NUM'])
+        print(
+            ':',
+            'Total number of rows:'.ljust(padding),
+            self.total_user_push, '\n')
+        print(
+            ':',
+            'Время на подключение:'.ljust(padding),
+            self.stop_connection_time - self.start_connection_time, '\n')
         print(':', 'Средний rps:'.ljust(padding), average_rps, '\n')
-        print(':', 'Время окончания теста:'.ljust(padding), datetime.datetime.today(), '\n')
+        print(
+            ':',
+            'Время окончания теста:'.ljust(padding),
+            datetime.datetime.today(), '\n')
 
     def entr_point(self):
         start_gen = perf_counter()
-        self.gen_users()  # Создаются пользователи, подключения и подготавливаются неизменяемые части строк
+        self.gen_users()
         end_gen = perf_counter()
-        logging.warning(f'Generated users {len(self.users)} in {end_gen-start_gen} seconds')
+        logging.warning(
+            f'Generated users {len(self.users)}
+            in {end_gen-start_gen} seconds')
 
         self.start_connection_time = perf_counter()
         self.connect_users()  # Пользователи подключаются к базе
         self.stop_connection_time = perf_counter()
         while threading.active_count() > 2:
-                continue
+            continue
         self.pbar.close()
-        
+
         self.start_insertion_time = perf_counter()
         try:
             if self.configuration['INTERVAL'] == 'loops':
                 logging.debug(f'start loops')
-                self.loops()  
+                self.loops()
             else:
                 logging.debug(f'start interval')
                 self.interval()
@@ -251,6 +303,7 @@ class SpectatorTesting:
             self.last_insertion_time = perf_counter()
             while threading.active_count() > 2:
                 continue
+            self.pbar.close()
             self.metrics()
 
 
@@ -263,6 +316,7 @@ def main():
     logging.warning(f'test worked in {stop_test-start_test} seconds')
     return 0
 
+
 def main_main(configuration):
     start_test = perf_counter()
     test = SpectatorTesting(configuration=configuration)
@@ -270,6 +324,7 @@ def main_main(configuration):
     stop_test = perf_counter()
     logging.warning(f'test worked in {stop_test-start_test} seconds')
     return 0
+
 
 if __name__ == '__main__':
     main()
